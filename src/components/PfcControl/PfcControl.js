32666346ccc34ad266e75e181b53da236494bfbd
shawnanderton <shawnanderton1@gmail.com>
2019-01-07 05:53:02 -0700

    testing

---
+++ src/components/PfcControl/PfcControl.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import { Button } from '@material-ui/core';
+
+const PfcControl = prop => {
+    let passed = { color: 'black', backgroundColor: '#40AF48', opacity: false ? 1 : 0.4 };
+    let caution = { color: 'black', backgroundColor: '#E8A543', opacity: false ? 1 : 0.4 };
+    let failed = { color: 'black', backgroundColor: '#B20909', opacity: true ? 1 : 0.4 };
+    let na = { color: 'black', backgroundColor: '#31B0D5', opacity: false ? 1 : 0.4 };
+
+    const changed = (event, value) => {
+        console.log(value);
+        switch (value[0]) {
+            case 'Pass':
+                passed.opacity = 1;
+                break;
+            default:
+        }
+    };
+
+    return (
+        <div>
+            <Button color="primary">Test</Button>
+            <ToggleButtonGroup onChange={changed}>
+                <ToggleButton color="main" value="Pass">
+                    Pass
+                </ToggleButton>
+                <ToggleButton style={caution} value="Caution" selected={true}>
+                    Caution
+                </ToggleButton>
+
+                <ToggleButton style={failed} value="Fail">
+                    Fail
+                </ToggleButton>
+                <ToggleButton style={na} value="N/A">
+                    N/A
+                </ToggleButton>
+            </ToggleButtonGroup>
+        </div>
+    );
+};
+
+export default PfcControl;
---
+++ src/docs/examples/PfcControl/Example.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PfcControl from 'ps-react/PfcControl';
+
+export default function PfcControlExample() {
+    return <PfcControl />;
+}
